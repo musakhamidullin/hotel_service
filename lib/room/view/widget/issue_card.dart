c685fda2f2154df454383e858dc8dbbd864407bb
Musa <94803483+musakhamidullin@users.noreply.github.com>
2023-12-18 00:22:38 +0300

    rename file

--- lib/room/view/widget/issue_card.dart
+++ lib/room/view/widget/issue_card.dart
@@ -13,7 +13,7 @@ import '../../cubit/room_cubit.dart';
 import '../../data/models/department_info.dart';
 
 import '../room_page.dart';
-import 'gallery/gallery_widget.dart';
+import 'gallery/gallary_widget.dart';
 import 'mini_images.dart';
 import 'departments_list.dart';
 import 'issue_field.dart';
