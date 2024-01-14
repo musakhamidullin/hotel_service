d6a24ebc9cbc5b6ad160a2c6c880fdd44f7a41a1
Musa <94803483+musakhamidullin@users.noreply.github.com>
2024-01-14 22:15:10 +0300

    fixed textfield in chat

--- lib/room/view/widget/comments/comments_sheet.dart
+++ lib/room/view/widget/comments/comments_sheet.dart
@@ -2,14 +2,12 @@ import 'dart:convert';
 import 'dart:io';
 
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:image_picker/image_picker.dart';
 
 import '../../../../common/widgets/cash_memory_image_provider.dart';
 import '../../../../common/widgets/modals.dart';
 import '../../../cubit/room_cubit.dart';
 import '../../../data/models/issues.dart';
-import '../gallery/gallery_widget.dart';
 import '../issue_field.dart';
 import 'comment_gallery.dart';
 
@@ -30,6 +28,22 @@ class CommentsSheet extends StatefulWidget {
 
 class _CommentsSheetState extends State<CommentsSheet> {
   final TextEditingController _textEditingController = TextEditingController();
+
+  double _listViewHeight = 0;
+  double _space = 150;
+
+  @override
+  void initState() {
+    super.initState();
+    // добавляем слушатель на появление клавиатуры
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      final keyboardHeight = MediaQuery.of(context).viewInsets.bottom;
+      setState(() {
+        _listViewHeight =
+            MediaQuery.of(context).size.height - 150 - keyboardHeight;
+      });
+    });
+  }
 
   @override
   void dispose() {
@@ -58,83 +72,37 @@ class _CommentsSheetState extends State<CommentsSheet> {
       maxChildSize: 1,
       minChildSize: 0.4,
       initialChildSize: 1,
-      builder: (_, scrollController) => Padding(
-        padding: const EdgeInsets.only(top: 12, left: 16, right: 12),
-        child: Column(
-          children: [
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      builder: (_, scrollController) => Scaffold(
+        body: Padding(
+          padding: const EdgeInsets.only(top: 12, left: 16, right: 12),
+          child: SingleChildScrollView(
+            child: Column(
               children: [
-                Text(
-                  'Комментарии',
-                  style: theme.textTheme.titleLarge,
-                ),
-                FilledButton.tonal(
-                  onPressed: () => Navigator.pop(context),
-                  child: const Text('Закрыть'),
-                )
-              ],
-            ),
-            const Flexible(
-                child: Center(
-              child: Text('Здесь будут комментарии'),
-            )),
-            Column(
-              children: [
-                if (_images.isNotEmpty)
-                  SizedBox(
-                    height: 100,
-                    child: ListView.builder(
-                      itemCount: _images.length,
-                      scrollDirection: Axis.horizontal,
-                      itemBuilder: (context, i) => SizedBox(
-                        width: 80,
-                        height: 80,
-                        child: Stack(
-                          children: [
-                            Positioned.fill(
-                              child: Image(
-                                image: CacheMemoryImageProvider(
-                                  tag: _images[i],
-                                  img:
-                                      const Base64Decoder().convert(_images[i]),
-                                ),
-                                fit: BoxFit.cover,
-                              ),
-                            ),
-                            Positioned.fill(
-                              top: 6,
-                              right: 6,
-                              child: Align(
-                                alignment: Alignment.topRight,
-                                child: GestureDetector(
-                                  onTap: () {
-                                    setState(() {
-                                      _images
-                                          .removeWhere((e) => e == _images[i]);
-                                    });
-                                  },
-                                  child: DecoratedBox(
-                                    decoration: BoxDecoration(
-                                        shape: BoxShape.circle,
-                                        color: Colors.black87.withOpacity(0.5)),
-                                    child: Icon(
-                                      Icons.cancel_rounded,
-                                      color: Colors.white.withOpacity(0.8),
-                                      size: 34,
-                                    ),
-                                  ),
-                                ),
-                              ),
-                            )
-                          ],
-                        ),
-                      ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(
+                      'Комментарии',
+                      style: theme.textTheme.titleLarge,
                     ),
+                    FilledButton.tonal(
+                      onPressed: () => Navigator.pop(context),
+                      child: const Text('Закрыть'),
+                    )
+                  ],
+                ),
+                SizedBox(
+                  height: _listViewHeight,
+                  child: ListView.builder(
+                    controller: scrollController,
+                    shrinkWrap: true,
+                    itemCount: 100,
+                    itemBuilder: (context, i) => Text(i.toString()),
                   ),
+                ),
                 Row(
                   children: [
-                    Flexible(
+                    Expanded(
                         child: IssueTextField(
                       readOnly: false,
                       onTextChanged: (String text) {},
@@ -154,8 +122,8 @@ class _CommentsSheetState extends State<CommentsSheet> {
                   ],
                 ),
               ],
-            )
-          ],
+            ),
+          ),
         ),
       ),
     );
