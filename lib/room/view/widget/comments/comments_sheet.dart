31a7f7785eb40d443491d1332ffac6ad3c49dff3
Musa <94803483+musakhamidullin@users.noreply.github.com>
2024-01-14 23:22:34 +0300

    fix textfield

--- lib/room/view/widget/comments/comments_sheet.dart
+++ lib/room/view/widget/comments/comments_sheet.dart
@@ -4,7 +4,6 @@ import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
 
-import '../../../../common/widgets/cash_memory_image_provider.dart';
 import '../../../../common/widgets/modals.dart';
 import '../../../cubit/room_cubit.dart';
 import '../../../data/models/issues.dart';
@@ -29,22 +28,6 @@ class CommentsSheet extends StatefulWidget {
 class _CommentsSheetState extends State<CommentsSheet> {
   final TextEditingController _textEditingController = TextEditingController();
 
-  double _listViewHeight = 0;
-  double _space = 150;
-
-  @override
-  void initState() {
-    super.initState();
-    // добавляем слушатель на появление клавиатуры
-    WidgetsBinding.instance.addPostFrameCallback((_) {
-      final keyboardHeight = MediaQuery.of(context).viewInsets.bottom;
-      setState(() {
-        _listViewHeight =
-            MediaQuery.of(context).size.height - 150 - keyboardHeight;
-      });
-    });
-  }
-
   @override
   void dispose() {
     super.dispose();
@@ -67,63 +50,52 @@ class _CommentsSheetState extends State<CommentsSheet> {
   @override
   Widget build(BuildContext context) {
     final theme = Theme.of(context);
-    return DraggableScrollableSheet(
-      expand: false,
-      maxChildSize: 1,
-      minChildSize: 0.4,
-      initialChildSize: 1,
-      builder: (_, scrollController) => Scaffold(
-        body: Padding(
-          padding: const EdgeInsets.only(top: 12, left: 16, right: 12),
-          child: SingleChildScrollView(
-            child: Column(
+    return Scaffold(
+      body: Padding(
+        padding: const EdgeInsets.only(top: 12, left: 16, right: 12),
+        child: Column(
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: [
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Text(
-                      'Комментарии',
-                      style: theme.textTheme.titleLarge,
-                    ),
-                    FilledButton.tonal(
-                      onPressed: () => Navigator.pop(context),
-                      child: const Text('Закрыть'),
-                    )
-                  ],
+                Text(
+                  'Комментарии',
+                  style: theme.textTheme.titleLarge,
                 ),
-                SizedBox(
-                  height: _listViewHeight,
-                  child: ListView.builder(
-                    controller: scrollController,
-                    shrinkWrap: true,
-                    itemCount: 100,
-                    itemBuilder: (context, i) => Text(i.toString()),
-                  ),
-                ),
-                Row(
-                  children: [
-                    Expanded(
-                        child: IssueTextField(
-                      readOnly: false,
-                      onTextChanged: (String text) {},
-                      onClearPressed: () {},
-                      textEditingController: _textEditingController,
-                    )),
-                    IconButton(
-                        onPressed: () async => await _onSelectedCameraPressed(),
-                        icon: const Icon(Icons.photo_camera_outlined)),
-                    IconButton(
-                        onPressed: () {
-                          Modals.showBottomSheet(
-                              context, const CommentGallery());
-                        },
-                        icon: const Icon(Icons.attach_file)),
-                    IconButton(onPressed: () {}, icon: const Icon(Icons.mic))
-                  ],
-                ),
+                FilledButton.tonal(
+                  onPressed: () => Navigator.pop(context),
+                  child: const Text('Закрыть'),
+                )
               ],
             ),
-          ),
+            Expanded(
+              child: ListView.builder(
+                reverse: true,
+                itemCount: 100,
+                itemBuilder: (context, i) => Text(i.toString()),
+              ),
+            ),
+            Row(
+              children: [
+                Expanded(
+                    child: IssueTextField(
+                  readOnly: false,
+                  onTextChanged: (String text) {},
+                  onClearPressed: () {},
+                  textEditingController: _textEditingController,
+                )),
+                IconButton(
+                    onPressed: () async => await _onSelectedCameraPressed(),
+                    icon: const Icon(Icons.photo_camera_outlined)),
+                IconButton(
+                    onPressed: () {
+                      Modals.showBottomSheet(context, const CommentGallery());
+                    },
+                    icon: const Icon(Icons.attach_file)),
+                IconButton(onPressed: () {}, icon: const Icon(Icons.mic))
+              ],
+            ),
+          ],
         ),
       ),
     );
