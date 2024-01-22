e7d7a3128f06a0700dd1328f29d1dc7c697be70d
Dima <pandora-house@mail.ru>
2024-01-22 22:36:49 +0300

    merged

--- lib/room/view/widget/comments/view/widget/input_card.dart
+++ lib/room/view/widget/comments/view/widget/input_card.dart
@@ -1,8 +1,11 @@
+import 'dart:io';
+import 'dart:typed_data';
 
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:image_picker/image_picker.dart';
 
+import '../../../../../../common/widgets/cash_memory_image_provider.dart';
 import '../../cubit/comments_cubit.dart';
 import '../../data/models/message_value.dart';
 
@@ -16,6 +19,8 @@ class InputCard extends StatefulWidget {
 class _InputCardState extends State<InputCard> {
   final _textController = TextEditingController();
   var _messageValue = const MessageValue();
+
+  final ValueNotifier<bool> _canSend = ValueNotifier(false);
 
   @override
   void dispose() {
@@ -37,6 +42,7 @@ class _InputCardState extends State<InputCard> {
           ),
           onChanged: (value) {
             _messageValue = _messageValue.copyWith(text: value);
+            _canSend.value = _messageValue.canSend();
           },
           onSubmitted: (value) {
             _messageValue = _messageValue.copyWith(text: value);
@@ -44,15 +50,64 @@ class _InputCardState extends State<InputCard> {
             FocusManager.instance.primaryFocus?.unfocus();
             //todo clear after success send
             _textController.clear();
+            _messageValue = _messageValue.copyWith(text: '');
+            _canSend.value = _messageValue.canSend();
           },
         ),
-        InputButtons(
-          textEditingController: _textController,
-          onSend: () {
-            context.read<CommentsCubit>().sendMessage(_messageValue);
-            FocusManager.instance.primaryFocus?.unfocus();
-            //todo clear after success send
-            _textController.clear();
+        if (_messageValue.buffImages?.isNotEmpty ?? false)
+          SizedBox(
+            height: 100,
+            child: ListView(
+              scrollDirection: Axis.horizontal,
+              children: _messageValue.buffImages!
+                  .map(
+                    (e) => // Image(
+                        Padding(
+                      padding: const EdgeInsets.only(right: 4),
+                      child: Image(
+                        image: CacheMemoryImageProvider(
+                          tag: _messageValue.buffImages!.indexOf(e).toString(),
+                          img: e,
+                        ),
+                        fit: BoxFit.cover,
+                        width: 90,
+                      ),
+                    ),
+                  )
+                  .toList(),
+            ),
+          ),
+        ValueListenableBuilder(
+          valueListenable: _canSend,
+          builder: (context, value, child) {
+            return InputButtons(
+              canSend: value,
+              onPhotographed: (value) {
+                setState(() {
+                  _messageValue = _messageValue.copyWith(
+                      buffImages: [...?_messageValue.buffImages, value]);
+                });
+                _canSend.value = _messageValue.canSend();
+              },
+              onSend: () {
+                context.read<CommentsCubit>().sendMessage(_messageValue);
+                FocusManager.instance.primaryFocus?.unfocus();
+                //todo clear after success send
+                _textController.clear();
+                setState(() {
+                  _messageValue = const MessageValue();
+                });
+                _canSend.value = _messageValue.canSend();
+              },
+              onClear: () {
+                _textController.clear();
+                setState(() {
+                  _messageValue = const MessageValue();
+                });
+                _canSend.value = _messageValue.canSend();
+                FocusManager.instance.primaryFocus?.unfocus();
+              },
+            );
           },
         )
       ],
@@ -64,46 +119,26 @@ class InputButtons extends StatefulWidget {
   const InputButtons({
     super.key,
     required this.onSend,
-    required this.textEditingController,
+    required this.onClear,
+    required this.onPhotographed,
+    required this.canSend,
   });
 
   final Function() onSend;
-  final TextEditingController textEditingController;
+  final Function() onClear;
+  final Function(Uint8List) onPhotographed;
+  final bool canSend;
 
   @override
   State<InputButtons> createState() => _InputButtonsState();
 }
 
 class _InputButtonsState extends State<InputButtons> {
-  final List<String> _images = [];
-  var _hasText = false;
-
   Future<void> _onSelectedCameraPressed() async {
     final pickedImage =
         await ImagePicker().pickImage(source: ImageSource.camera);
-    if (pickedImage != null) {
-      setState(() {
-        _images.add(pickedImage.path);
-      });
-    }
-  }
-
-  @override
-  void initState() {
-    super.initState();
-    widget.textEditingController.addListener(_textListener);
-  }
-
-  @override
-  void dispose() {
-    widget.textEditingController.removeListener(_textListener);
-    super.dispose();
-  }
-
-  void _textListener() {
-    setState(() {
-      _hasText = widget.textEditingController.text.isNotEmpty;
-    });
+    if (pickedImage?.path == null) return;
+    widget.onPhotographed(File(pickedImage!.path).readAsBytesSync());
   }
 
   @override
@@ -130,17 +165,11 @@ class _InputButtonsState extends State<InputButtons> {
           icon: const Icon(Icons.mic),
         ),
         IconButton(
-          onPressed: _hasText ? widget.onSend : null,
+          onPressed: widget.canSend ? widget.onSend : null,
           icon: const Icon(Icons.send),
         ),
         IconButton(
-          onPressed: _hasText
-              ? () {
-                  //todo clear all images
-                  widget.textEditingController.clear();
-                  FocusManager.instance.primaryFocus?.unfocus();
-                }
-              : null,
+          onPressed: widget.canSend ? widget.onClear : null,
           icon: const Icon(Icons.clear),
         ),
       ],
